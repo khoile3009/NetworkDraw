793c16b46ba0d8da73c177b8f03501fc42793f37
Khoi Le <khoibaka@gmail.com>
2022-02-06 16:28:01 -0500

    Working movenode

--- src/Components/Interactive/Canvas.js
+++ src/Components/Interactive/Canvas.js
@@ -1,5 +1,5 @@
 import Node from "./Node";
-import { useState, useCallback, useReducer } from "react";
+import { useState, useCallback, useReducer, useRef } from "react";
 
 const width = 1000;
 const height = 600;
@@ -11,8 +11,25 @@ const createNode = (nodes) => {
     connections: [],
     top: Math.floor(Math.random() * height),
     left: Math.floor(Math.random() * width),
+    focused: false,
   };
   return newNodes;
+};
+
+const moveNode = (nodes, index, event, canvas) => {
+  console.log(canvas);
+  const left = event.pageX - canvas.offsetLeft;
+  const top = event.pageY - canvas.offsetTop;
+  console.log(
+    event.pageX,
+    event.pageY,
+    event.target.offsetLeft,
+    event.target.offsetTop,
+    left,
+    top
+  );
+  nodes[index] = { ...nodes[index], left: left, top: top };
+  return nodes;
 };
 
 const nodeReducer = (state, action) => {
@@ -21,15 +38,35 @@ const nodeReducer = (state, action) => {
   switch (action.type) {
     case "addNode":
       newState = { ...state, nodes: [...state.nodes, createNode(state.nodes)] };
+      break;
+    case "focusNode":
+      newState = { ...state, focusedIndex: action.index };
+      break;
+    case "unfocusNode":
+      newState = {
+        ...state,
+        focusedIndex: null,
+      };
+      break;
+    case "moveNode":
+      newState = {
+        ...state,
+        nodes: moveNode(state.nodes, action.index, action.event, action.canvas),
+      };
+      break;
+    default:
+      newState = state;
   }
   return newState;
 };
 
 const initialState = {
   nodes: [],
+  focusedIndex: null,
 };
 
 function Canvas() {
+  const canvas = useRef(null);
   const [state, dispatch] = useReducer(nodeReducer, initialState);
 
   return (
@@ -42,19 +79,40 @@ function Canvas() {
         Add node
       </button>
       <div
+        ref={canvas}
         style={{
           background: "lightblue",
           height: height + "px",
           width: width + "px",
           position: "relative",
         }}
-        onClick={(event) => {
-          console.log(event);
+        onMouseUp={(event) => {
+          if (event.button == 0) dispatch({ type: "unfocusNode" });
+        }}
+        onMouseMove={(event) => {
+          if (
+            state.focusedIndex != null &&
+            event != undefined &&
+            event.button == 1
+          ) {
+            dispatch({
+              type: "moveNode",
+              index: state.focusedIndex,
+              event: event,
+              canvas: canvas.current,
+            });
+          }
         }}
       >
-        {state.nodes.map((node) => {
+        {state.nodes.map((node, index) => {
           return (
-            <Node key={node.name} top={node["top"]} left={node["left"]}></Node>
+            <Node
+              key={node.name}
+              {...node}
+              focusNode={() => {
+                dispatch({ type: "focusNode", index: index });
+              }}
+            ></Node>
           );
         })}
       </div>
--- src/Components/Interactive/Node.js
+++ src/Components/Interactive/Node.js
@@ -1,20 +1,31 @@
+// TODO: Move this to a separate env file
+const size = 40;
+
 function Node(props) {
   let top = props["top"];
   let left = props["left"];
   return (
     <div
+      onMouseDown={(event) => {
+        if (event.button == 0) props.focusNode();
+      }}
+      onContextMenu={(event) => {
+        event.preventDefault();
+        console.log(event);
+      }}
       style={{
         background: "white",
-        width: "30px",
-        height: "30px",
+        color: "black",
+        width: size + "px",
+        height: size + "px",
         borderRadius: "100%",
         border: "2px solid black",
         position: "absolute",
-        left: left + "px",
-        top: top + "px",
+        left: left - size / 2 + "px",
+        top: top - size / 2 + "px",
       }}
     >
-      {props["name"]}
+      <p>{props["name"]}</p>
     </div>
   );
 }
